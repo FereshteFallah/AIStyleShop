d46a271640dd7b9433832957c0d498ea05e8cce0
FereshteFallah <en.f.fallah@gmail.com>
2025-08-17 18:45:46 +0400

    Initial commit with project files and README

---
+++ smartshopping/orders/context_processors.py
@@ -0,0 +1,11 @@
+# cart/context_processors.py
+from .models import Order
+
+def cart_item_count(request):
+    if request.user.is_authenticated:
+        # گرفتن سفارش فعلی با وضعیت 'pending'
+        order = Order.objects.filter(user=request.user, status='pending').first()
+        count = order.items.count() if order else 0
+    else:
+        count = 0
+    return {'cart_item_count': count}
